35008f5c30fc8e2ff0ffa848106fac6cfaacf177
sauravmaropost <saurav.kumar@maropost.com>
2019-06-28 12:43:42 +0530

    Aded delete permissions

--- GoLangCode/main.go
+++ GoLangCode/main.go
@@ -13,7 +13,8 @@ func handleHome(w http.ResponseWriter, r *http.Request){
 }
 func handleRequests(){
 	myRouter := mux.NewRouter().StrictSlash(true)
-	methods := handlers.AllowedHeaders([]string{"GET","POST","PUT","DELETE"})
+	handers:= handlers.AllowedHeaders([]string{"X-Requested-With","Content-Type","Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET","POST","PUT","DELETE","HEAD","DEBUG","PATCH","OPTIONS"})
 	origins := handlers.AllowedOrigins([]string{"*"})
 	myRouter.HandleFunc("/",handleHome).Methods("GET")
 	myRouter.HandleFunc("/user",user.Alluser).Methods("GET")
@@ -21,7 +22,7 @@ func handleRequests(){
 	myRouter.HandleFunc("/user/{email}/{title}/{message}",user.PostUser).Methods("POST")
 	myRouter.HandleFunc("/user/{email}",user.GetUserPost).Methods("GET")
 	myRouter.HandleFunc("/user/{id}",user.DeleteUserPost).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000",handlers.CORS(methods,origins)(myRouter)))
+	log.Fatal(http.ListenAndServe(":8000",handlers.CORS(methods,origins,handers)(myRouter)))
 }
 func main(){
 	fmt.Println("JSMPJ Corporation")
--- vuecode/postcss.config.js
+++ vuecode/postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: {
     autoprefixer: {}
-  }
+  },
+  devServer: {
+    proxy: 'http://localhost:8000/',
+}
 }
